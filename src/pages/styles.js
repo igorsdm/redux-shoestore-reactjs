fa84b7b52787bd36346628e8f4401881f6418e4e
Igor Moraes <igor@portalqualis.com.br>
2020-02-18 17:29:30 -0300

    base project

---
+++ src/pages/styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #fff;
+  text-align: center;
+  margin: 10% 10%;
+  min-height: 200px;
+  align-items: center;
+`;
+
+export const Title = styled.h1`
+  color: #192841;
+`;
